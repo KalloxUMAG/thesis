b1383dfac6771f16d363cbafc1146c0c627d48e1
Kallo <d34d.mc@gmail.com>
2023-02-23 00:16:08 -0300

    Epitopes error fixed

--- dags/scripts/db_conn/epitopes.py
+++ dags/scripts/db_conn/epitopes.py
@@ -2,7 +2,7 @@ from scripts.db_conn.models import Epitope
 
 def get_epitope_id(name, db):
     #db = get_db()
-    data = db.query(Epitope).filter(Epitope.name == name).all()
+    data = db.query(Epitope).filter(Epitope.name == name).first()
     #close_db(db)
     if data == None:
         return -1
